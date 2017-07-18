5115133fdba3ca2b5622775bc69573fd2fc3ac41
hodongman <arshavin3@naver.com>
2017-07-18 16:47:06 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+7CHICKEN PROJECT
+
+
+1. git clone ...
+2. yarn install / npm install
+3. yarn start / npm start
+
+
+if you want to build yarn build / npm run build
+
+ok bye bye
+
---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src='https://images-gmi-pmc.edge-generalmills.com/549e441a-eed1-4e39-8ad3-6ae60ab3ad7f.jpg' className="App-logo" alt="logo" />
+          <h2>7 Chicken!!</h2>
+        </div>
+        <p className="App-intro">
+          칠킨즈 Hello World!! 개발자는 치쏘가 치맥보다 좋습니다.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
